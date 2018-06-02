41f4c6006abb8eba8c24c8c112bd47cea5f97936
blackopss <36933564+blackopss@users.noreply.github.com>
2018-06-02 03:45:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ function play(guild, song) {
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 
-const adminprefix = ".";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
+const adminprefix = "1";//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 const devs = ['431150885549113344'];//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , 🔕#4777 'CODES SERVER'
